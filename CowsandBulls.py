2b9333d4ea846a4ad3a3394f2d65a0101ea9850d
David Russell <45084308+dmruss@users.noreply.github.com>
2020-01-31 12:21:08 -0700

    Update CowsandBulls.py

--- CowsandBulls.py
+++ CowsandBulls.py
@@ -3,38 +3,34 @@ import random
 def cow_bull():
     counter = 0       #counter is 0
     n = str(random.randint(1000,9999))   #generate 4 digit number
-    s = n                               #duplicate random number
-    print 'Welcome to the Cows and Bulls Game!'
-    print 'A cow is a digit in the correct place'
-    print 'A bull is a digit in the wrong place'
-
+                              #duplicate random number
+    print ('Welcome to the Cows and Bulls Game!')
+    print ('A cow is a digit in the correct place')
+    print ('A bull is a digit in the wrong place')
+    print (n)
     while True:
-        
+      
+        s = n    
         g = str(input('Enter a four digit number: '))   #ask for a number
         cow = 0
         bull = 0     #set cow and bull to 0
         counter += 1                  #up counter by one each guess
-        if n[0] == g[0]:         #checking each individual place for cow
-            cow += 1        #if correct add one to cow
-            g = g.replace(g[0], ".")       #remove digit so isn't counted as bull
-        if n[1] == g[1]:
+       
+        for x in range(4):
+          if n[x] == g[x]:
             cow += 1
-            g = g.replace(g[1], ".")
-        if n[2] == g[2]:
-            cow += 1
-            g = g.replace(g[2], ".")
-        if n[3] == g[3]:
-            cow += 1
-            g = g.replace(g[3], ".")
+            g = g.replace(g[x], ",")
+       
         if cow == 4:                 #checking for winning number
-            print 'You guessed it! The number was %s' %n
-            print 'It took you %d tries.' %counter
+            print ('You guessed it! The number was %s' %n)
+            print ('It took you %d tries.' %counter)
             break
-        for digit in g:          #checking for bulls
-            if digit in n:
+        for digit in n:          #checking for bulls
+            if digit in g:
                 bull += 1
-                s = s.replace(digit, ".")           #replace digit in duplicate random number with "." so not double counted
-        print "Cows: %d. Bulls: %d" %(cow, bull)
+                s = s.replace(digit, ".") 
+                g = g.replace(digit, ",")          #replace digit in duplicate random number with "." so not double counted
+        print ("Cows: %d. Bulls: %d" %(cow, bull))
         
     
     
